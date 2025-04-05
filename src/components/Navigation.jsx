819341545b1a5539a9b0d81b6194f5171670e126
DineshReddy1812 <dineshgowravarapu218@gmail.com>
2025-04-05 11:34:52 +0530

    logo added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navigation = () => {
   return (
     <nav>
-        
+        <div className="navlogo">
+            <img src='/images/logo.png' alt='logo' />
+        </div>
     </nav>
   )
 }
